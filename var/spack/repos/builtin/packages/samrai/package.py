5cb80572e17195c57f578ef64cc12dc508600ec5
Brian T.N. Gunney <gunney1@llnl.gov>
2023-11-22 14:00:03 -0800

    Make SAMRAI build compatible with cmake and autotools.

--- var/spack/repos/builtin/packages/samrai/package.py
+++ var/spack/repos/builtin/packages/samrai/package.py
@@ -3,122 +3,164 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack.build_systems.autotools import AutotoolsBuilder
+from spack.build_systems.cmake import CMakeBuilder
 from spack.package import *
-from spack.pkg.builtin.boost import Boost
 
-
-class Samrai(AutotoolsPackage):
-    """SAMRAI (Structured Adaptive Mesh Refinement Application Infrastructure)
+class Samrai(CMakePackage, AutotoolsPackage, CudaPackage):
+  """SAMRAI (Structured Adaptive Mesh Refinement Application Infrastructure)
     is an object-oriented C++ software library enables exploration of
     numerical, algorithmic, parallel computing, and software issues
     associated with applying structured adaptive mesh refinement
     (SAMR) technology in large-scale parallel application development.
-
     """
 
-    homepage = "https://computing.llnl.gov/projects/samrai"
-    url = "https://computing.llnl.gov/projects/samrai/download/SAMRAI-v3.11.2.tar.gz"
-    list_url = homepage
-    tags = ["radiuss"]
+  homepage = "https://computing.llnl.gov/projects/samrai"
+  git = "https://github.com/LLNL/samrai.git"
+  tags = ["radiuss"]
+  maintainers = ["gunney1"]
 
-    version("3.12.0", sha256="b8334aa22330a7c858e09e000dfc62abbfa3c449212b4993ec3c4035bed6b832")
-    version("3.11.5", sha256="6ec1f4cf2735284fe41f74073c4f1be87d92184d79401011411be3c0671bd84c")
-    version("3.11.4", sha256="fa87f6cc1cb3b3c4856bc3f4d7162b1f9705a200b68a5dc173484f7a71c7ea0a")
-    # Version 3.11.3 permissions don't allow downloading
-    version("3.11.2", sha256="fd9518cc9fd8c8f6cdd681484c6eb42114aebf2a6ba4c8e1f12b34a148dfdefb")
-    version("3.11.1", sha256="14317938e55cb7dc3eca21d9b7667a256a08661c6da988334f7af566a015b327")
-    version("3.10.0", sha256="8d6958867f7165396459f4556e439065bc2cd2464bcfe16195a2a68345d56ea7")
-    version("3.9.1", sha256="ce0aa9bcb3accbd39c09dd32cbc9884dc00e7a8d53782ba46b8fe7d7d60fc03f")
-    version("3.8.0", sha256="0fc811ca83bd72d238f0efb172d466e80e5091db0b78ad00ab6b93331a1fe489")
-    version("3.7.3", sha256="19eada4f351a821abccac0779fde85e2ad18b931b6a8110510a4c21707c2f5ce")
-    version("3.7.2", sha256="c20c5b12576b73a1a095d8ef54536c4424517adaf472d55d48e57455eda74f2d")
-    version(
-        "3.6.3-beta", sha256="7d9202355a66b8850333484862627f73ea3d7620ca84cde757dee629ebcb61bb"
-    )
-    version(
-        "3.5.2-beta", sha256="9a591fc962edd56ea073abd13d03027bd530f1e61df595fae42dd9a7f8b9cc3a"
-    )
-    version(
-        "3.5.0-beta", sha256="3e10c55d7b652b6feca902ce782751d4b16c8ad9d4dd8b9e2e9ec74dd64f30da"
-    )
-    version(
-        "3.4.1-beta", sha256="5aadc813b75b65485f221372e174a2691e184e380f569129e7aa4484ca4047f8"
-    )
-    version(
-        "3.3.3-beta", sha256="c07b5dc8d56a8f310239d1ec6be31182a6463fea787a0e10b54a3df479979cac"
-    )
-    version(
-        "3.3.2-beta", sha256="430ea1a77083c8990a3c996572ed15663d9b31c0f8b614537bd7065abd6f375f"
-    )
-    version("2.4.4", sha256="33242e38e6f4d35bd52f4194bd99a014485b0f3458b268902f69f6c02b35ee5c")
+  def url_for_version(self, version):
+    if version.up_to(1) >= Version(4):
+      dirUrl = f'https://github.com/LLNL/SAMRAI/releases/download'
+    else:
+      dirUrl = f'https://computing.llnl.gov/projects/samrai/download'
+    url = dirUrl + f'/SAMRAI-v{version.dotted}.tar.gz'
+    return url
 
-    # Debug mode reduces optimization, includes assertions, debug symbols
-    # and more print statements
-    variant(
-        "debug", default=False, description="Compile with reduced optimization and debugging on"
-    )
-    variant("silo", default=False, description="Compile with support for silo")
-    variant("shared", default=False, description="Build shared libraries")
+  version("develop", branch="develop")
+  version("4.2.0", sha256="34a256c99e29bee6dee017253a18cf1ed6435f0376e666e235e9092a895fabc6")
+  version("4.1.2", sha256="2860c693ba495613ce0ea04cc72eb8749519a770d520ef0a83a54ecff3d515e3")
+  version("4.1.1", sha256="ceb6e5b3b1587c45b8c41971f252974e40eae3d5fe9ad93269d969ea4acc2637")
+  version("4.1.0", sha256="633968bf1d4ff9c0f1f0c31591f9bb665f8252231a9ca03b54c3b498239a5815")
+  version("3.12.0", sha256="b8334aa22330a7c858e09e000dfc62abbfa3c449212b4993ec3c4035bed6b832")
+  version("3.11.2", sha256="fd9518cc9fd8c8f6cdd681484c6eb42114aebf2a6ba4c8e1f12b34a148dfdefb")
+  version("3.11.1", sha256="14317938e55cb7dc3eca21d9b7667a256a08661c6da988334f7af566a015b327")
+  version("3.11.0", sha256="4033f9fd02e09990bd564d226d84df5bae6baa16e51ab29e8a607a6d593a3806")
+  version("3.10.0", sha256="59eeeb2d3b7e52ea45efb037272e36666940682407ae159dab9066aad527cfa0")
+  version("3.9.0", sha256="1ffdbdf454986fdfd8a599f4b0a14c17e2e9563fe6041d91203c88c5174fd117")
+  version("3.8.1", sha256="963e6d6faec52cdb86a14f7b8564f6641fc2241e004ca54405f6bc5f7b593066")
 
-    depends_on("mpi")
-    depends_on("zlib")
-    depends_on("hdf5+mpi")
-    depends_on("m4", type="build")
-    depends_on("boost@:1.64.0", when="@3.0.0:3.11", type="build")
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants, when="@3.0.0:3.11.99", type="build")
-    depends_on("silo+mpi", when="+silo")
+  build_system(
+    conditional("cmake", when="@3.13:"),
+    conditional("autotools", when="@:3.12"),
+    default="cmake",
+  )
 
-    # don't build SAMRAI 3+ with tools with gcc
-    patch("no-tool-build.patch", when="@3.0.0:%gcc")
+  # debug variant is only for autotools build.
+  # With CMake, use build_type=Debug, provided by CMakePackage.
+  variant("debug", default=False, when='build_system=autotools',
+          description="Configure with debug options.")
 
-    # 2.4.4 needs a lot of patches to fix ADL and performance problems
-    patch(
-        "https://github.com/IBAMR/IBAMR/releases/download/v0.3.0/ibamr-samrai-fixes.patch?full_index=1",
-        sha256="1d088b6cca41377747fa0ae8970440c20cb68988bbc34f9032d5a4e6aceede47",
-        when="@2.4.4",
-    )
+  variant("shared", default=False, description="Build shared libraries")
 
-    def configure_args(self):
-        options = []
+  # After some version (I'm guessing 4), we support conduit.
+  # TODO: verify and correct.
+  variant("conduit", default=False, when='@4:',
+          description="Enable support for conduit")
 
-        options.extend(
-            [
-                "--with-CXX=%s" % self.spec["mpi"].mpicxx,
-                "--with-CC=%s" % self.spec["mpi"].mpicc,
-                "--with-F77=%s" % self.spec["mpi"].mpifc,
-                "--with-M4=%s" % self.spec["m4"].prefix,
-                "--with-hdf5=%s" % self.spec["hdf5"].prefix,
-                "--with-zlib=%s" % self.spec["zlib"].prefix,
-                "--without-blas",
-                "--without-lapack",
-                "--with-hypre=no",
-                "--with-petsc=no",
-            ]
-        )
+  variant('boost', default=False, description='Enable support for boost', when='@3.0:3.11')
+  variant("cuda", default=False, description="Enable support for cuda")
+  variant("hdf5", default=True, description="Enable support for hdf5")
+  variant("hip", default=False, description="Enable support for hip")
+  variant("hypre", default=False, description="Enable support for hypre")
+  variant("mpi", default=True, description="Enable support for MPI")
+  variant("openmp", default=False, description="Enable support for OPENMP")
+  variant("petsc", default=False, description="Enable support for petsc")
+  variant("raja", default=False, description="Enable support for raja")
+  variant("silo", default=False, description="Enable support for silo")
+  variant("sundials", default=False, description="Enable support for sundials")
+  variant("umpire", default=False, description="Enable support for umpire")
 
-        # SAMRAI 2 used templates; enable implicit instantiation
-        if self.spec.satisfies("@:3"):
-            options.append("--enable-implicit-template-instantiation")
+  # Couldn't find zlib in SAMRAI cmake builds.
+  # Assuming it's only in autotools build.
+  depends_on("zlib", when='build_system=autotools')
 
-        if "+debug" in self.spec:
-            options.extend(["--disable-opt", "--enable-debug"])
-        else:
-            options.extend(["--enable-opt", "--disable-debug"])
+  depends_on('boost', when='+boost')
 
-        if "+silo" in self.spec:
-            options.append("--with-silo=%s" % self.spec["silo"].prefix)
+  depends_on("mpi", when="+mpi")
 
-        if "+shared" in self.spec:
-            options.append("--enable-shared")
+  # Bring in dependencies for variants that don't use MPI.
+  for tpl in 'raja cuda hip umpire'.split():
+    with when(f'+{tpl}'):
+      depends_on(tpl)
 
-        if self.spec.satisfies("@3.0:3.11"):
-            options.append("--with-boost=%s" % self.spec["boost"].prefix)
+  # Pass mpi variants to dependency packages that use them.
+  for tpl in 'hdf5 hypre petsc sundials silo'.split():
+    with when(f'+{tpl}'):
+      depends_on(f"{tpl}+mpi", when="+mpi")
+      depends_on(f"{tpl}~mpi", when="~mpi")
 
-        return options
+  with when("+conduit"):
+    # If ~mpi, use a non-parmetis conduit, because parmetis requires mpi.
+    depends_on("conduit+mpi+parmetis", when="+mpi")
+    depends_on("conduit~mpi~parmetis", when="~mpi")
 
-    def setup_dependent_build_environment(self, env, dependent_spec):
-        if self.spec.satisfies("@3.12:"):
-            env.append_flags("CXXFLAGS", self.compiler.cxx11_flag)
+
+class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
+  def cmake_args(self):
+    options = []
+
+    sharedLibMode = 'ON' if '+shared' in self.spec else 'OFF'
+    options.append('-DBUILD_SHARED_LIBS=' + sharedLibMode)
+
+    # Third-party libs handled with either
+    # ENABLE_TPL=OFF or TPL_DIR=/path/to/tpl/install.
+    for tpl in 'conduit cuda hdf5 hypre raja petsc silo sundials'.split():
+      TPL = tpl.upper()
+      if f'~{tpl}' in self.spec:
+        options.append(f'-DENABLE_{TPL}=OFF')
+      else:
+        options.append(f'-D{TPL}_DIR=' + self.spec[tpl].prefix)
+
+    # Umpire has unconventional capitalization in cmake command.
+    if '~umpire' in self.spec:
+      options.append(f'-DENABLE_UMPIRE=OFF')
+    else:
+      options.append(f'-Dumpire_DIR=' + self.spec['umpire'].prefix)
+
+    if '+hdf5' in self.spec:
+      options.append(f'-DHDF5_C_COMPILER_EXECUTABLE={self.spec["hdf5"].prefix}/bin/h5cc')
+
+    # Third-party libs handled with either
+    # ENABLE_TPL=OFF or ENABLE_TPL=ON
+    for tpl in 'openmp'.split():
+      tplMode = 'ON' if f'+{tpl}' in self.spec else 'OFF'
+      TPL = tpl.upper()
+      options.append(f'-DENABLE_{TPL}={tplMode}')
+
+    return options
+
+
+class AutotoolsBuilder(spack.build_systems.autotools.AutotoolsBuilder):
+  def configure_args(self):
+    options = []
+
+    # SAMRAI 2 used templates; enable implicit instantiation
+    if self.spec.satisfies("@:2"):
+      options.append("--enable-implicit-template-instantiation")
+
+    if "+shared" in self.spec:
+      options.append("--enable-shared")
+    else:
+      options.append("--disable-shared")
+
+    if "+debug" in self.spec:
+      options.extend('--disable-opt --enable-debug'.split())
+    else:
+      options.extend('--enable-opt --disable-debug'.split())
+
+    # Third-parth libs specified as either
+    # --with-tpl=/path/to/tpl/install or --without-tpl
+    for tpl in 'boost hdf5 hypre petsc silo sundials'.split():
+      if f'+{tpl}' in self.spec:
+        options.append(f'--with-{tpl}=' + self.spec[tpl].prefix)
+      else:
+        options.append(f'--without-{tpl}')
+
+    options.extend('--without-blas --without-lapack'.split())
+
+    if '+zlib' in self.spec:
+      options.append(f'--with-zlib={self.spec["zlib"].prefix}')
+
+    return options
